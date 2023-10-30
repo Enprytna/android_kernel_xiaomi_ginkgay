5ec5d35fd47d4f424bb294735bec17502f85eb92
Enprytna <e.endipriyatna@gmail.com>
2023-10-30 07:27:18 +0000

    build.sh: Update build script

--- build.sh
+++ build.sh
@@ -1,69 +1,94 @@
 #!/bin/bash
 #
 # Compile script for QuicksilveR kernel
-# Copyright (C) 2020-2021 Adithya R.
+# Copyright (C) 2020-2023 Adithya R.
 
 SECONDS=0 # builtin bash timer
 ZIPNAME="QuicksilveRV2-ginkgo-KSU-$(date '+%Y%m%d-%H%M').zip"
-TC_DIR="$HOME/tc/xRageTC-clang"
-AK3_DIR="$HOME/android/AnyKernel3"
+TC_DIR="$HOME/tc/clang-r498229b"
+GCC_64_DIR="$HOME/tc/aarch64-linux-android-4.9"
+GCC_32_DIR="$HOME/tc/arm-linux-androideabi-4.9"
+AK3_DIR="$HOME/AnyKernel3"
 DEFCONFIG="vendor/ginkgo-perf_defconfig"
+
+export PATH="$TC_DIR/bin:$PATH"
+export KBUILD_BUILD_USER="enn"
+export KBUILD_BUILD_HOST="enprytna"
+
+if ! [ -d "${TC_DIR}" ]; then
+echo "Clang not found! Cloning to ${TC_DIR}..."
+if ! git clone --depth=1 -b main https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86 $HOME/tc; then
+echo "Cloning failed! Aborting..."
+exit 1
+fi
+fi
+
+if ! [ -d "${GCC_64_DIR}" ]; then
+echo "GCC_64 not found! Cloning to ${GCC_64_DIR}..."
+if ! git clone --depth=1 -b lineage-19.1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9.git ${GCC_64_DIR}; then
+echo "Cloning failed! Aborting..."
+exit 1
+fi
+fi
+
+if ! [ -d "${GCC_32_DIR}" ]; then
+echo "GCC_32 not found! Cloning to ${GCC_32_DIR}..."
+if ! git clone --depth=1 -b lineage-19.1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9.git ${GCC_32_DIR}; then
+echo "Cloning failed! Aborting..."
+exit 1
+fi
+fi
+
+MAKE_PARAMS="O=out ARCH=arm64 CC=clang LD=ld.lld AR=llvm-ar AS=llvm-as NM=llvm-nm \
+	OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip \
+	CLANG_TRIPLE=aarch64-linux-gnu- \
+	CROSS_COMPILE=$GCC_64_DIR/bin/aarch64-linux-android- \
+	CROSS_COMPILE_ARM32=$GCC_32_DIR/bin/arm-linux-androideabi-"
 
 if test -z "$(git rev-parse --show-cdup 2>/dev/null)" &&
    head=$(git rev-parse --verify HEAD 2>/dev/null); then
 	ZIPNAME="${ZIPNAME::-4}-$(echo $head | cut -c1-8).zip"
 fi
 
-export PATH="$TC_DIR/bin:$PATH"
-
-if ! [ -d "$TC_DIR" ]; then
-echo "xRageTC clang not found! Cloning to $TC_DIR..."
-if ! git clone -q -b main --depth=1 https://github.com/xyz-prjkt/xRageTC-clang $TC_DIR; then
-echo "Cloning failed! Aborting..."
-exit 1
-fi
-fi
-
-export KBUILD_BUILD_USER=endi
-export KBUILD_BUILD_HOST=enprytna
-
 if [[ $1 = "-r" || $1 = "--regen" ]]; then
-make O=out ARCH=arm64 $DEFCONFIG savedefconfig
-cp out/defconfig arch/arm64/configs/$DEFCONFIG
-exit
+   make $MAKE_PARAMS $DEFCONFIG savedefconfig
+   cp out/defconfig arch/arm64/configs/$DEFCONFIG
+   echo -e "\nSuccessfully regenerated defconfig at arch/arm64/configs/$DEFCONFIG"
+   exit
 fi
 
 if [[ $1 = "-c" || $1 = "--clean" ]]; then
-rm -rf out
+   rm -rf out
+   echo "Cleaned output folder"
 fi
 
 mkdir -p out
-make O=out ARCH=arm64 $DEFCONFIG
+make $MAKE_PARAMS $DEFCONFIG
 
 echo -e "\nStarting compilation...\n"
-make -j$(nproc --all) O=out ARCH=arm64 CC=clang LD=ld.lld AR=llvm-ar AS=llvm-as NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_ARM32=arm-linux-gnueabi- Image.gz-dtb dtbo.img
+make -j$(nproc --all) $MAKE_PARAMS Image.gz-dtb dtbo.img 2> >(tee error.log >&2) || exit $?
 
-if [ -f "out/arch/arm64/boot/Image.gz-dtb" ] && [ -f "out/arch/arm64/boot/dtbo.img" ]; then
+kernel="out/arch/arm64/boot/Image.gz-dtb"
+dtbo="out/arch/arm64/boot/dtbo.img"
+
+if [ ! -f "$kernel" ] || [ ! -f "$dtbo" ]; then
+	echo -e "\nCompilation failed!"
+	exit 1
+fi
+
 echo -e "\nKernel compiled succesfully! Zipping up...\n"
 if [ -d "$AK3_DIR" ]; then
-cp -r $AK3_DIR AnyKernel3
-elif ! git clone -q https://github.com/Enprytna/AnyKernel3; then
-echo -e "\nAnyKernel3 repo not found locally and cloning failed! Aborting..."
-exit 1
+	cp -r $AK3_DIR AnyKernel3
+	git -C AnyKernel3 checkout master &> /dev/null
+elif ! git clone -q https://github.com/Enprytna/AnyKernel3 -b master; then
+	echo -e "\nAnyKernel3 repo not found locally and couldn't clone from GitHub! Aborting..."
+	exit 1
 fi
-cp out/arch/arm64/boot/Image.gz-dtb AnyKernel3
-cp out/arch/arm64/boot/dtbo.img AnyKernel3
-rm -f *zip
+cp $kernel $dtbo AnyKernel3
+rm -rf out/arch/arm64/boot
 cd AnyKernel3
-git checkout master &> /dev/null
 zip -r9 "../$ZIPNAME" * -x '*.git*' README.md *placeholder
 cd ..
 rm -rf AnyKernel3
-rm -rf out/arch/arm64/boot
 echo -e "\nCompleted in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s) !"
-echo "Zip: $ZIPNAME"
-curl --upload-file $ZIPNAME https://temp.sh/$ZIPNAME; echo
-else
-echo -e "\nCompilation failed!"
-exit 1
-fi
+echo "$(realpath $ZIPNAME)"
